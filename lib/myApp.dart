c4081a54b5e807d971bf0d82c6855dda37518f91
Home <sun-kyu.kim@aya.yale.edu>
2019-09-20 23:29:55 -0400

    commit

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 import 'loginPage.dart';
 import 'package:provider/provider.dart';
 import './models/FormModel.dart';
+import './stepper.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Forms Demo',
-      home: ChangeNotifierProvider<FormModel>(
-          builder: (_) => FormModel('', null, '', '', null, null, '', '', null,
-              new DateTime.now(), null, null),
-          child: LoginPage()),
-    );
+    return new MaterialApp(
+        theme: new ThemeData(
+          primarySwatch: Colors.lightGreen,
+        ),
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Steppers'),
+          ),
+          body: new StepperBody(),
+        ));
   }
 }
